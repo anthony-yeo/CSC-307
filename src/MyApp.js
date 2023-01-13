399972ca0ec06412a7d1857f4555fd5046893f3f
antknees <47506456+antknees@users.noreply.github.com>
2023-01-13 13:49:06 -0800

    Added form

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Table from './Table';
+import Form from './Form';
 
 // const characters = [
 //     {
@@ -21,24 +22,7 @@ import Table from './Table';
 // ];
 
 function MyApp() { 
-    const [characters, setCharacters] = useState([
-        {
-            name: 'Charlie',
-            job: 'Janitor',
-        },
-        {
-            name: 'Mac',
-            job: 'Bouncer',
-        },
-        {
-            name: 'Dee',
-            job: 'Aspiring actress',
-        },
-        {
-            name: 'Dennis',
-            job: 'Bartender',
-        },
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter (index) {
         const updated = characters.filter((character, i) => {
@@ -50,6 +34,7 @@ function MyApp() {
     return (
         <div className="container">
             <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form />
         </div> 
     );
 }
