83e7d13ad4e62d8bfaba8c76623cfab2dd34e7b7
antknees <47506456+antknees@users.noreply.github.com>
2023-01-13 13:36:24 -0800

    Added Delete Users

--- src/Table.js
+++ src/Table.js
@@ -12,33 +12,14 @@ function TableHeader(){
 }
 
 function TableBody(props){
-    
-    // return (
-    //     <tbody>
-    //         <tr>
-    //             <td>Charlie</td>
-    //             <td>Janitor</td>
-    //         </tr>
-    //         <tr>
-    //             <td>Mac</td>
-    //             <td>Bouncer</td>
-    //         </tr>
-    //         <tr>
-    //             <td>Dee</td>
-    //             <td>Aspiring actress</td>
-    //         </tr>
-    //         <tr>
-    //             <td>Dennis</td>
-    //             <td>Bartender</td>
-    //         </tr>
-    //     </tbody>
-    // );
-
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key = {index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick = {() => props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         );
     });
@@ -54,7 +35,7 @@ function Table(props) {
     return (
         <table>
             <TableHeader />
-            <TableBody characterData = {props.characterData} />
+            <TableBody characterData = {props.characterData} removeCharacter = {props.removeCharacter} />
         </table>
     );  
 }
