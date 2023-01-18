7f4eb8bbebd5ed27b4a708f01386fed8170c68ff
antknees <47506456+antknees@users.noreply.github.com>
2023-01-18 14:25:11 -0800

    Finish Step 5

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,11 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
-import axios from './axios';
-import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+//import React, {useState, useEffect} from 'react';
 
-function MyApp() { 
+function MyApp(){ 
     const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetchAll().then( result => {
+            if (result)
+                setCharacters(result);
+        });
+    }, [] );  
 
     function removeOneCharacter (index) {
         const updated = characters.filter((character, i) => {
@@ -14,34 +21,48 @@ function MyApp() {
         setCharacters(updated);
     }
 
-    function updateList(person){
-        setCharacters([...characters, person]);
-    }
-
     return (
         <div className="container">
             <Table characterData={characters} removeCharacter={removeOneCharacter} />
             <Form handleSubmit={updateList}/>
         </div> 
     );
+
+    function updateList (person) {
+        makePostCall(person).then( result => {
+            if (result && result.status === 200)
+                setCharacters([...characters, person]);
+        });
+    }
+
+    
+    
+    async function fetchAll(){
+        try{
+            const response = await axios.get('http://localhost:5000/users');
+            return response.data.users_list;
+        }
+        catch (error){
+            console.log(error);
+            return false;
+        }
+    }
+    
+    async function makePostCall(person){
+        try{
+            const response = await axios.post('http://localhost:5000/users', person)
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+            return false;  
+        }
+    }
+    
+    
+    
 }
 
-async function fetchAll(){
-    try{
-        const response = await axios.get('http://localhost:5000/users');
-        return response.data.users_list;
-    }
-    catch (error){
-        console.log(error);
-        return false;
-    }
-}
 
-useEffect(() => {
-    fetchAll().then( result => {
-        if (result)
-            setCharacters(result);
-    });
-}, [] );  
  
 export default MyApp;
