92a024f0cde1c01462bcc894d086bb2bbcded4be
antknees <47506456+antknees@users.noreply.github.com>
2023-01-25 21:11:59 -0800

    Revert "Merge branch 'dev' into main"
    This reverts commit fa1f2598f565bb28a0b7f67abea7648daec77696, reversing
    changes made to 133840d72968b69c912cb75cdf1f0ef377e1c957.

--- src/Table.js
+++ src/Table.js
@@ -6,7 +6,6 @@ function TableHeader(){
             <tr>
                 <th>Name</th>
                 <th>Job</th>
-                <th>ID</th>
             </tr>
         </thead>
     );
@@ -18,7 +17,6 @@ function TableBody(props){
             <tr key = {index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
-                <td>{row.id}</td>
                 <td>
                     <button onClick = {() => props.removeCharacter(index)}>Delete</button>
                 </td>
